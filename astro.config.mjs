c7230c1b4e618201e3d9712da674d685e69bd2f8
ENDER9090-ESP <joseantoniofranciscomogollon@gmail.com>
2025-09-03 17:06:57 -0600

    workflow

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+        site: 'https://ENDER9090-ESP.github.io',
+        base: 'Portafolio1',
+});
