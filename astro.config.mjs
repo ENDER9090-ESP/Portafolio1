381f7e9057f0587786901a39e8d38e7f72107b65
ENDER9090-ESP <joseantoniofranciscomogollon@gmail.com>
2025-09-18 13:36:54 -0600

    correcion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-        site: 'https://ENDER9090-ESP.github.io',
-        base: 'Portafolio1',
+    site: 'https://ENDER9090-ESP.github.io',
+    base: '/Portafolio1',
 });
